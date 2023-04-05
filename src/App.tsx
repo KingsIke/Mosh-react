b2965b94801ee720634724fc23a8f4ce2ac44781
KINGS=IKE <ogbonnayakingsike@gmail.com>
2023-04-05 23:57:00 +0100

    zod

--- src/App.tsx
+++ src/App.tsx
@@ -8,50 +8,52 @@ import produce from 'immer'
 import NavBar from './components/NavBar/NavBar'
 import Card from './components/Card/Card'
 import Expandable from './components/Expendable/Expandable'
+import Form from './components/Form/Form'
 
 
 function App() {
 
   //UPDATING WITH Immer
 
-  const [bugs, setBugs] = useState([
-    { id: 1, title: 'Bug1', fixed: false },
-    { id: 2, title: 'Bug2', fixed: false },
+  // const [bugs, setBugs] = useState([
+  //   { id: 1, title: 'Bug1', fixed: false },
+  //   { id: 2, title: 'Bug2', fixed: false },
 
-  ])
+  // ])
 
-  const handleClickBug = () => {
-    // console.log(bugs)
-    // setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug))
-    // console.log(bugs)
+  // const handleClickBug = () => {
+  //   // console.log(bugs)
+  //   // setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug))
+  //   // console.log(bugs)
 
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id === 1);
-      if (bug) bug.fixed = true
-    }))
-  }
+  //   setBugs(produce(draft => {
+  //     const bug = draft.find(bug => bug.id === 1);
+  //     if (bug) bug.fixed = true
+  //   }))
+  // }
 
 
   /// Sharing Props
 
-  const [cartItem, setCartItem] = useState(['Product1', 'Product2'])
+  // const [cartItem, setCartItem] = useState(['Product1', 'Product2'])
 
-  const items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ]
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+  // const items = [
+  //   'New York',
+  //   'San Francisco',
+  //   'Tokyo',
+  //   'London',
+  //   'Paris'
+  // ]
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item)
+  // }
 
-  const [alertVisible, setAlertVisible] = useState(false)
+  // const [alertVisible, setAlertVisible] = useState(false)
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <Form />
+      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
       <div>
         {alertVisible && <Alert onClose={() => setAlertVisible(false)}> My Alert</Alert>}
         <Button onClick={() => setAlertVisible(true)} color='secondary'>My Button </Button>
@@ -60,18 +62,21 @@ function App() {
         {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'} </p>)}
         <button onClick={handleClickBug}> click Me</button>
 
-      </div>
+      </div> */}
 
 
       {/* cartItems */}
-      <NavBar cartItemsCount={cartItem.length} />
+      {/* <NavBar cartItemsCount={cartItem.length} />
       <Card cartItems={cartItem} onClear={() => setCartItem([])} />
 
       <div style={{ margin: '10px' }}>
         <Expandable maxChars={10}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta fuga veniam voluptatem eveniet quaerat amet nobis omnis eos iusto. Illum accusamus animi sequi cupiditate deserunt suscipit est corporis corrupti nostrum reiciendis eaque, facere impedit nesciunt ratione repudiandae ea quibusdam enim? Doloremque totam maxime facilis adipisci minima mollitia tempora numquam laboriosam sequi? Unde tenetur delectus aspernatur sunt natus placeat, voluptatem veniam eaque eveniet alias minima voluptates possimus. Quos eos inventore nihil quam sed modi, cumque, magni laudantium laborum non enim, aut iste consequuntur? Veritatis iste assumenda animi nulla exercitationem voluptatibus repellendus quasi fugit necessitatibus id! Enim facere quaerat voluptatem dolor commodi.
         </Expandable>
-      </div>
+      </div> */}
+
+
+
     </div>
 
   )
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,150 @@
-import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
+// import * as z from 'zod';
+// import { zodResolver } from '@hookform/resolvers/zod'
 
-export const Form = () => {
+// const schema = z.object({
+//     name: z.string().min(3),
+//     age: z.number().min(18)
+// })
+
+// type FormData = z.infer<typeof schema>
+
+export default function Form() {
+    //For UseForm
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+    //     {
+    //     resolver: zodResolver(schema)
+    // }
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data)
+    }
+
+
+
+
     return (
-        <form action="">
-            <div className="mb-3">
-                <label htmlFor="" className="form-label">
-                </label>
-                <input type="text" className="form-control" />
-            </div>
-        </form>
+        <>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name
+                    </label>
+                    <input {...register('name', { required: true, minLength: 3 })} id='name' type="text" className="form-control" />
+                    {errors.name?.type === 'required' && (
+                        <p className='text-danger'>{errors.name.message}</p>
+                    )}
+
+
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="age" className="form-label">Age
+                    </label>
+                    <input {...register('age')} id='age' type="number" className="form-control" />
+                    {errors.age?.type === 'required' && (
+                        <p className='text-danger'>{errors.age.message}</p>
+                    )}
+
+
+                </div>
+                <button className="btn btn-primary" type='submit'>Submit</button>
+            </form>
+        </>
     )
 }
+
+
+
+
+
+
+ //import React, { FormEvent, useRef, useState } from 'react'
+// import { FieldValues, useForm } from 'react-hook-form'
+// import * as z from 'zod';
+// import { zodResolver } from '@hookform/resolvers/zod'
+
+// const schema = z.object({
+//     name: z.string().min(3),
+//     age: z.number().min(18)
+// })
+
+// type FormData = z.infer<typeof schema>
+
+// export default function Form() {
+//     //For UseForm
+//     const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+//         resolver: zodResolver(schema)
+//     })
+//     // console.log(register('nam e'))
+//     // console.log(formState.errors)
+
+//     const onSubmit = (data: FieldValues) => {
+//         console.log(data)
+//     }
+
+
+//     // For useRef
+//     // const nameRef = useRef<HTMLInputElement>(null)
+//     // const ageRef = useRef<HTMLInputElement>(null)
+//     // const person = { name: '', age: '' }
+
+
+//     //FOR useState and onChange
+//     // const [name, setName] = useState('')
+//     // const [age, setAge] = useState('')
+//     // const handleName = (e: any) => {
+//     //     setName(e.target.value)
+//     // }
+//     // const handleAge = (e: any) => {
+//     //     setAge(e.target.value)
+//     // }
+
+//     // const handleSubmit = (event: FormEvent) => {
+//     //     event.preventDefault()
+
+//     // if (nameRef.current !== null) {
+//     //     person.name = nameRef.current.value
+//     // }
+//     // if (ageRef.current !== null) {
+//     //     person.age = ageRef.current.value
+//     // }
+//     // console.log(person)
+
+//     // for UseState
+//     // const form = { name, age }
+//     // console.log(form)
+//     // }
+//     return (
+//         <>
+//             {/* <form onClick={handleSubmit}> */}
+//             <form onSubmit={handleSubmit(onSubmit)}>
+
+//                 <div className="mb-3">
+//                     <label htmlFor="name" className="form-label">Name
+//                     </label>
+//                     <input {...register('name')} id='name' type="text" className="form-control" />
+//                     {errors.name?.type === 'required' && (
+//                         <p className='text-danger'>{errors.name.message}</p>
+//                     )}
+
+//                     {/* <input ref={nameRef} id='name' type="text" className="form-control" value={name} onChange={handleName} /> */}
+//                 </div>
+
+//                 <div className="mb-3">
+//                     <label htmlFor="age" className="form-label">Age
+//                     </label>
+//                     <input {...register('age')} id='age' type="number" className="form-control" />
+//                     {errors.age?.type === 'required' && (
+//                         <p className='text-danger'>{errors.age.message}</p>
+//                     )}
+
+//                     {/* <input ref={ageRef} id='age' type="number" className="form-control" value={age} onChange={handleAge} /> */}
+//                 </div>
+//                 <button className="btn btn-primary" type='submit'>Submit</button>
+//             </form>
+//         </>
+//     )
+// }
+
